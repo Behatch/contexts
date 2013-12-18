9d8aa4e1ed55c4028662cc0f59f96aa9e9df3066
Sanpi <sanpi@homecomputing.fr>
2013-12-18 13:54:03 +0100

    Check style

--- fixtures/www/browser/auth.php
+++ fixtures/www/browser/auth.php
@@ -2,35 +2,32 @@
 
 session_start();
 
-$username = "gabriel";
-$password = "30091984";
+$username = 'gabriel';
+$password = '30091984';
 
-//log out
-if(isset($_GET['logout']))
-{
-  unset($_SESSION["login"]);
-  echo "Logged out<br /><a href='" . $_SERVER['PHP_SELF'] . "'>Login</a>";
-  exit;
+if (isset($_GET['logout'])) {
+    unset($_SESSION['login']);
+    echo "Logged out<br /><a href='" . $_SERVER['PHP_SELF'] . "'>Login</a>";
 }
-
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || !isset($_SESSION["login"]))
-{
-  header("WWW-Authenticate: Basic realm=\"Test\"");
-  header("HTTP/1.0 401 Unauthorized");
-  $_SESSION["login"] = true;
-  echo "NONE SHALL PASS !";
-  exit;
+elseif (
+    !isset($_SERVER['PHP_AUTH_USER'])
+    || !isset($_SERVER['PHP_AUTH_PW'])
+    || !isset($_SESSION['login'])
+) {
+    header('WWW-Authenticate: Basic realm="Test"');
+    header('HTTP/1.0 401 Unauthorized');
+    $_SESSION['login'] = true;
+    echo 'NONE SHALL PASS !';
 }
-else
-{
-  if($_SERVER['PHP_AUTH_USER'] == $username && $_SERVER['PHP_AUTH_PW'] == $password)
-  {
-    echo "Successfuly logged in";
-  }
-  else
-  {
-    unset($_SESSION["login"]);
-    header("Location: " . $_SERVER['PHP_SELF']);
-  }
+else {
+    if(
+        $_SERVER['PHP_AUTH_USER'] == $username
+        && $_SERVER['PHP_AUTH_PW'] == $password
+    ) {
+        echo 'Successfuly logged in';
+    }
+    else {
+        unset($_SESSION['login']);
+        header('Location: ' . $_SERVER['PHP_SELF']);
+    }
 }
-?>
