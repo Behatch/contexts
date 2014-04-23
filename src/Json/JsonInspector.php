47c580a10f0707913d6d747d28531ec9cf8e5ef3
Timothée Barray <tim@amicalement-web.net>
2014-04-23 16:17:50 +0200

    Extract json manipulation to dedicated class
    Let people use the json manipulation without affect JsonContext

---
+++ src/Json/JsonInspector.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Sanpi\Behatch\Json;
+
+use JsonSchema\RefResolver;
+use JsonSchema\Validator;
+use JsonSchema\Uri\UriRetriever;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class JsonInspector
+{
+    private $evaluationMode;
+
+    private $accessor;
+
+    public function __construct($evaluationMode)
+    {
+        $this->evaluationMode = $evaluationMode;
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    public function evaluate(Json $json, $expression)
+    {
+        if ($this->evaluationMode === 'javascript') {
+            $expression = str_replace('->', '.', $expression);
+        }
+
+        try {
+            return $json->read($expression, $this->accessor);
+        } catch (\Exception $e) {
+            throw new \Exception(sprintf('Failed to evaluate expression "%s"', $expression));
+        }
+    }
+
+    public function validate(Json $json, JsonSchema $schema)
+    {
+        return $schema
+            ->resolve(new RefResolver(new UriRetriever))
+            ->validate($json, new Validator)
+        ;
+    }
+}
