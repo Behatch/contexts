2178eaa44ce4a83668f8d3e04705ee0d69e46a68
Sanpi <sanpi@homecomputing.fr>
2015-04-08 23:46:40 +0200

    Simplify atoum configuration

--- .atoum.php
+++ .atoum.php
@@ -1,11 +1,3 @@
 <?php
-use \mageekguy\atoum;
 
-$runner->addTestsFromDirectory(__DIR__.'/tests');
-
-$script
-    ->addDefaultReport()
-        ->addField(new atoum\report\fields\runner\result\logo())
-;
-
-$script->bootstrapFile(__DIR__ . DIRECTORY_SEPARATOR . '.atoum.bootstrap.php');
+$runner->addTestsFromDirectory(__DIR__ . '/tests/units');
