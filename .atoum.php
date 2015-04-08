983e0168086ff98fb22bb7d50a6ee3e41d0d0cba
Sanpi <sanpi@homecomputing.fr>
2015-04-09 00:15:59 +0200

    Generate code coverage

--- .atoum.php
+++ .atoum.php
@@ -1,3 +1,12 @@
 <?php
 
 $runner->addTestsFromDirectory(__DIR__ . '/tests/units');
+
+$script->addDefaultReport();
+
+$xunitWriter = new \atoum\writers\file(__DIR__ . '/atoum.xunit.xml');
+
+$xunitReport = new \atoum\reports\asynchronous\xunit();
+$xunitReport->addWriter($xunitWriter);
+
+$runner->addReport($xunitReport);
