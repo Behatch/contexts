86d0b77e31b0f65cecf824e252ccd3cf6abed567
Sanpi <sanpi@homecomputing.fr>
2017-04-01 22:49:13 +0200

    Enable deprecation notice for class aliases

--- src/class_aliases/HttpCall/Request.php
+++ src/class_aliases/HttpCall/Request.php
@@ -1,5 +1,5 @@
 <?php
 
-@trigger_error('The Sanpi\\Behatch\\Request class is deprecated since version 2.6 and will be removed in 3.0. Use the Behatch\\Request class instead.', E_USER_DEPRECATED);
+trigger_error('The Sanpi\\Behatch\\Request class is deprecated since version 2.6 and will be removed in 3.0. Use the Behatch\\Request class instead.', E_USER_DEPRECATED);
 
 class_alias('\\Behatch\\Request', '\\Sanpi\\Behatch\\Request');
