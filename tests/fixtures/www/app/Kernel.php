ea9ab631ef396b19fdf70f7884471726f47f338e
Terje Bråten <terje@surelygroup.com>
2019-03-27 08:11:24 +0000

    Update Kernel so we can make do without a MicroKernelTrait

--- tests/fixtures/www/app/Kernel.php
+++ tests/fixtures/www/app/Kernel.php
@@ -2,17 +2,15 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
-
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     private $projectDir;
@@ -34,6 +32,29 @@ class Kernel extends BaseKernel
     public function getLogDir()
     {
         return $this->getProjectDir().'/var/log';
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(function (ContainerBuilder $container) use ($loader) {
+            $container->loadFromExtension('framework', [
+                'router' => [
+                    'resource' => 'kernel::loadRoutes',
+                    'type' => 'service',
+                ],
+            ]);
+
+            if ($this instanceof EventSubscriberInterface) {
+                $container->register('kernel', static::class)
+                    ->setSynthetic(true)
+                    ->setPublic(true)
+                    ->addTag('kernel.event_subscriber');
+            }
+
+            $this->configureContainer($container, $loader);
+
+            $container->addObjectResource($this);
+        });
     }
 
     public function registerBundles()
